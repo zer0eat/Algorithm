5264a5939b48a60ad089ff8079602d7f90e9baf7
zer0eat <zer00eat@gmail.com>
2022-12-24 00:09:46 +0900

    221224 Algorithm

---
+++ 221224_1_N과M7_BOJ15656/221224_N과M7_BOJ15656.py
@@ -0,0 +1,24 @@
+# N과M7_BOJ15656
+
+# input 열기
+import sys
+sys.stdin = open('input.txt')
+
+def NM():
+    if len(ans) == M:                           # ans 리스트에 M개의 요소가 들어있다면
+        print(*ans)                             # ans를 출력하고
+        return                                  # return한다
+    for i in lst:                               # lst를 반복해서
+        ans.append(i)                           # ans에 i를 append하고
+        NM()                                    # NM 함수를 실행하고
+        ans.pop()                               # ans에서 i를 pop한다
+
+# input 받기
+N, M = map(int, sys.stdin.readline().split())   # N 수열의 개수 / M 중복 없이 고를 개수
+
+lst = list(map(int, sys.stdin.readline().split()))  # input 받은 수열을 lst에 저장한다
+lst.sort()                                      # lst를 오름차순으로 정렬한 후
+
+ans = []                                        # 정답을 저장할 빈 리스트 생성
+
+NM()                                            # NM 함수 실행
