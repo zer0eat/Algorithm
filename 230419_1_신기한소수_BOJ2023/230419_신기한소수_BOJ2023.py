375589894c72e79930750b1df932046f723b406e
zer0eat <zer00eat@gmail.com>
2023-04-19 00:08:10 +0900

    230419 Algorithm

--- 230419_1_신기한소수_BOJ2023/230419_신기한소수_BOJ2023.py
+++ 230419_1_신기한소수_BOJ2023/230419_신기한소수_BOJ2023.py
@@ -11,6 +11,7 @@ N = int(input())                                        # N 자리의 수
 prime = [2, 3, 5, 7]                                    # N이 1일 때 소수의 리스트를 저장
 tmp = []                                                # prime보다 한자리 큰 소수를 저장할 빈 리스트 생성
 cnt = 1                                                 # 자리수를 셀 변수 생성
+
 while cnt < N:                                          # 자리수가 N이 될때
     for i in prime:                                     # prime 리스트에 담긴 숫자를 반복해서
         for j in range(1, 10, 2):                       # 끝자리가 짝수라면 소수가 될 수 없으므로 홀수만 반복한다
@@ -24,5 +25,6 @@ while cnt < N:                                          # 자리수가 N이 될�
         cnt += 1                                        # cnt에 1을 추가해서 자리수를 1 늘리고
         prime = tmp[:]                                  # prime을 tmp로 바꿔 저장한다
         tmp = []                                        # tmp는 비워준다
+
 for i in prime:                                         # N자리의 소수를 구했다면 prime을 반복해서
     print(i)                                            # N자리 소수를 출력한다
