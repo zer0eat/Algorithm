a69f724149e0c0c406f6518f9f494b020756ba80
zer0eat <zer00eat@gmail.com>
2024-04-13 23:58:57 +0900

    240413 Algorithm

--- 240413_1_별 찍기 - 7_BOJ2444/240413_별 찍기 - 7_BOJ2444.py
+++ 240413_1_별 찍기 - 7_BOJ2444/240413_별 찍기 - 7_BOJ2444.py
@@ -6,8 +6,8 @@ sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
 # input 받기
-N = int(input())
-for i in range(1, N):
-    print(' '*(N-i) + '*'*(2*i-1))
-for i in range(N, 0, -1):
-    print(' '*(N-i) + '*'*(2*i-1))
+N = int(input())                    # 별을 찍기 위한 줄을 input 받고
+for i in range(1, N):               # 별을 찍을 줄을 증가하는 만큼 반복해서
+    print(' '*(N-i) + '*'*(2*i-1))  # 증가하는 별을 출력한다
+for i in range(N, 0, -1):           # 별을 찍을 줄을 감소하는 만큼 반복해서
+    print(' '*(N-i) + '*'*(2*i-1))  # 감소하는 별을 출력한다
