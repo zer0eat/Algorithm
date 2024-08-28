75136cf89353e005a49e51154deac60067af9db8
zer0eat <zer00eat@gmail.com>
2024-08-28 22:35:43 +0900

    240828 Algorithm

---
+++ 240828_1_UCPC에서 가장 쉬운 문제 번호는_BOJ25311/240828_UCPC에서 가장 쉬운 문제 번호는_BOJ25311.py
@@ -0,0 +1,10 @@
+# UCPC에서 가장 쉬운 문제 번호는_BOJ25311
+
+# input.txt 열기
+import sys
+sys.stdin = open('input.txt')
+input = sys.stdin.readline
+
+# input 받기
+y = int(input())    # 출제 년도를 input 받고
+print('A')          # 가장 쉬운 문제의 번호를 출력한다
