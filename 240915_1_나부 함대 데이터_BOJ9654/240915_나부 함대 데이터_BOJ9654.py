bdb8907f45191bfc94082ad9cce24e5e94b3d5b8
zer0eat <zer00eat@gmail.com>
2024-09-15 13:44:01 +0900

    240915 Algorithm

---
+++ 240915_1_나부 함대 데이터_BOJ9654/240915_나부 함대 데이터_BOJ9654.py
@@ -0,0 +1,14 @@
+# 나부 함대 데이터_BOJ9654
+
+# input.txt 열기
+import sys
+sys.stdin = open('input.txt')
+input = sys.stdin.readline
+
+# input 받기
+print('SHIP NAME      CLASS          DEPLOYMENT IN SERVICE')    # 나부 함대 데이터를 출력한다
+print('N2 Bomber      Heavy Fighter  Limited    21        ')    # 나부 함대 데이터를 출력한다
+print('J-Type 327     Light Combat   Unlimited  1         ')    # 나부 함대 데이터를 출력한다
+print('NX Cruiser     Medium Fighter Limited    18        ')    # 나부 함대 데이터를 출력한다
+print('N1 Starfighter Medium Fighter Unlimited  25        ')    # 나부 함대 데이터를 출력한다
+print('Royal Cruiser  Light Combat   Limited    4         ')    # 나부 함대 데이터를 출력한다
