2f26b0421b3c7e12c115576ce6dd18f769581041
zer0eat <zer00eat@gmail.com>
2024-11-01 22:48:37 +0900

    241101 Algorithm

---
+++ 241101_1_콜센터_BOJ5339/241101_콜센터_BOJ5339.py
@@ -0,0 +1,18 @@
+# 콜센터_BOJ5339
+
+# input.txt 열기
+import sys
+sys.stdin = open('input.txt')
+input = sys.stdin.readline
+
+# input 받기
+print("     /~\\")
+print("    ( oo|")
+print("    _\=/_")
+print("   /  _  \\")
+print("  //|/.\|\\\\")
+print(" ||  \ /  ||")
+print("============")
+print("|          |")
+print("|          |")
+print("|          |")   # 콜센터를 출력한다
