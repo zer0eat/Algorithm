bea73c1edad7603f3399d82b1428d07279d0b646
zer0eat <zer00eat@gmail.com>
2024-11-02 22:49:36 +0900

    241102 Algorithm

---
+++ 241102_1_Rook_BOJ2393/241102_Rook_BOJ2393.py
@@ -0,0 +1,21 @@
+# Rook_BOJ2393
+
+# input.txt 열기
+import sys
+sys.stdin = open('input.txt')
+input = sys.stdin.readline
+
+# input 받기
+print("  ___  ___  ___")
+print("  | |__| |__| |")
+print("  |           |")
+print("   \\_________/")
+print("    \\_______/")
+print("     |     |")
+print("     |     |")
+print("     |     |")
+print("     |     |")
+print("     |_____|")
+print("  __/       \\__")
+print(" /             \\")
+print("/_______________\\") # 룩을 출력한다
