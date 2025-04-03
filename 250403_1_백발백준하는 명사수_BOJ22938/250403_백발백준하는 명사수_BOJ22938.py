6a7ecdebde4ca5d804c0761ac5f5f6e95b538906
zer0eat <zer00eat@gmail.com>
2025-04-03 20:41:18 +0900

    250403 Algorithm

--- 250403_1_백발백준하는 명사수_BOJ22938/250403_백발백준하는 명사수_BOJ22938.py
+++ 250403_1_백발백준하는 명사수_BOJ22938/250403_백발백준하는 명사수_BOJ22938.py
@@ -8,7 +8,7 @@ input = sys.stdin.readline
 # input 받기
 x1, y1, r1 = map(int, input().split())      # 원의 중심과 반지름을 input 받고
 x2, y2, r2 = map(int, input().split())      # 다른 원의 중심과 반지름을 input 받고
-if (x1-x2)**2 + (y1-y2)**2 <= (r1+r2)**2:   # 겹치는 부분이 있다면
+if (x1-x2)**2 + (y1-y2)**2 < (r1+r2)**2:    # 겹치는 부분이 있다면
     print('YES')                            # YES를 출력하고
 else:                                       # 겹치는 부분이 없다면
     print('NO')                             # NO를 출력한다
