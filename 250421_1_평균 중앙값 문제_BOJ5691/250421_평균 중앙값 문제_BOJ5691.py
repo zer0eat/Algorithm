63468bfffd1a0cc1489805ed3189b22417d4fecf
Youngsik Kim <youngsikkim@Youngsikui-MacBookPro.local>
2025-04-21 22:11:16 +0900

    250421 Algorithm

--- 250421_1_평균 중앙값 문제_BOJ5691/250421_평균 중앙값 문제_BOJ5691.py
+++ 250421_1_평균 중앙값 문제_BOJ5691/250421_평균 중앙값 문제_BOJ5691.py
@@ -10,4 +10,4 @@ while 1:                                # break가 나올 때까지 반복해서
     a, b = map(int, input().split())    # 두 수를 input 받고
     if a == 0 and b == 0:               # 두 수 모두 0이라면
         break                           # while문을 종료하고
-    print(a-abs(a-b))                   # a가 중앙값이고 b가 최대값일 때 최소값을 출력한다
+    print(a-abs(a-b))                   # a가 중앙값이고 b가 최대값일 때 최소값을 출력한다 ß
